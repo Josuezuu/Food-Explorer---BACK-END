7b6887306606b64425bb5918e681744b79c5405a
josue <josue-zu@hotmail.com>
2023-06-17 12:59:17 -0300

    fix editdish

--- src/controllers/DishesController.js
+++ src/controllers/DishesController.js
@@ -82,56 +82,158 @@ class DishesController {
     return response.status(200).json();
   }
 
+  // async update(request, response) {
+  //   const { id } = request.params;
+  //   const {
+  //     name,
+  //     description,
+  //     price,
+  //     category_name,
+  //     ingredients,
+  //     avatar_dish,
+  //   } = request.body;
+
+  //   if (avatar_dish == undefined) {
+  //     throw new AppError("Este prato não existe.", 404);
+  //   }
+  //   // const { filename: avatar_dish } = request.file;
+
+  //   // const filename = await diskStorage.saveFile(avatar_dish);
+  //   const axios = require("axios");
+
+  //   const sharp = require("sharp");
+
+  //   async function getDishImage(dishId) {
+  //     const dish = await knex("dishes")
+  //       .select("avatar_dish")
+  //       .where({ id: dishId })
+  //       .first();
+
+  //     if (!dish) {
+  //       throw new Error("O prato não foi encontrado.");
+  //     }
+
+  //     const imageBlob = dish.avatar_dish;
+  //     let processedImage;
+
+  //     if (typeof imageBlob == "object" && imageBlob instanceof Buffer) {
+  //       try {
+  //         processedImage = await sharp(imageBlob).resize(800, 600).toBuffer();
+  //       } catch (error) {
+  //         throw new Error("Falha ao processar a imagem do prato.");
+  //       }
+  //     } else {
+  //       throw new Error(
+  //         "O campo avatar_dish não contém um objeto BLOB válido."
+  //       );
+  //     }
+
+  //     return processedImage;
+  //   }
+
+  //   const avatar = await getDishImage(id);
+  //   console.log(avatar);
+  //   // const avatar = await knex("dishes")
+  //   //   .where({ avatar_dish: avatar_dish })
+  //   //   .first();
+  //   // console.log(avatar);
+
+  //   // const dish = await knex("dishes").where({ id }).first();
+  //   // const category = await knex("category")
+  //   //   .where({ name: category_name })
+  //   //   .first();
+
+  //   // if (!avatar_dish) {
+  //   //   dish.avatar_dish = dish.avatar_dish;
+  //   // } else {
+  //   //   dish.avatar_dish = filename;
+  //   // }
+  //   // if (!dish) {
+  //   //   throw new AppError("Este prato não existe.", 404);
+  //   // }
+
+  //   // dish.name = name ?? dish.name;
+  //   // dish.description = description ?? dish.description;
+  //   // dish.price = price ?? dish.price;
+  //   // dish.avatar_dish = filename ?? dish.avatar_dish;
+
+  //   // await knex("dishes").where({ id: dish.id }).update({
+  //   //   name: dish.name,
+  //   //   description: dish.description,
+  //   //   price: dish.price,
+  //   //   avatar_dish: dish.avatar_dish,
+  //   //   category_id: category.id,
+  //   // });
+
+  //   // await knex("ingredients").where({ dish_id: dish.id }).delete();
+
+  //   // const ingredientsList = ingredients.split(",");
+
+  //   // for (let i = 0; i < ingredientsList.length; i++) {
+  //   //   const ingredient = ingredientsList[i].trim();
+
+  //   //   if (ingredient.id) {
+  //   //     await knex("ingredients")
+  //   //       .where({ id: ingredient.id })
+  //   //       .update({ name: ingredient });
+  //   //   } else {
+  //   //     await knex("ingredients").insert({
+  //   //       dish_id: dish.id,
+  //   //       name: ingredient,
+  //   //     });
+  //   //   }
+  //   // }
+
+  //   return response.status(500).json();
+  // }
+
   async update(request, response) {
     const { id } = request.params;
     const { name, description, price, category_name, ingredients } =
       request.body;
-    const { filename: avatar_dish } = request.file;
-
-    const filename = await diskStorage.saveFile(avatar_dish);
+    const avatar_dish = request.file; // Alteração aqui
 
     const dish = await knex("dishes").where({ id }).first();
-    const category = await knex("category")
-      .where({ name: category_name })
-      .first();
 
     if (!dish) {
       throw new AppError("Este prato não existe.", 404);
     }
 
-    dish.name = name ?? dish.name;
-    dish.description = description ?? dish.description;
-    dish.price = price ?? dish.price;
-    dish.avatar_dish = filename ?? dish.avatar_dish;
+    const checkDish = await knex("dishes")
+      .where({ name })
+      .whereNot({ id })
+      .first();
 
-    await knex("dishes").where({ id: dish.id }).update({
-      name: dish.name,
-      description: dish.description,
-      price: dish.price,
-      avatar_dish: dish.avatar_dish,
+    if (checkDish) {
+      throw new AppError("Este nome de prato já está em uso.", 401);
+    }
+
+    const category = await knex("category")
+      .where({ name: category_name })
+      .first();
+
+    if (!category) {
+      throw new AppError("Esta categoria não existe.", 400);
+    }
+
+    // Verifica se o arquivo foi enviado na requisição
+    let filename = dish.avatar_dish;
+    if (avatar_dish) {
+      filename = await diskStorage.saveFile(avatar_dish);
+    }
+
+    // Resto do código...
+
+    // Atualiza os dados do prato existente no banco de dados com as informações fornecidas
+    await knex("dishes").where({ id }).update({
+      name,
+      description,
+      price,
+      avatar_dish: filename,
       category_id: category.id,
     });
 
-    await knex("ingredients").where({ dish_id: dish.id }).delete();
-
-    const ingredientsList = ingredients.split(",");
-
-    for (let i = 0; i < ingredientsList.length; i++) {
-      const ingredient = ingredientsList[i].trim();
-
-      if (ingredient.id) {
-        await knex("ingredients")
-          .where({ id: ingredient.id })
-          .update({ name: ingredient });
-      } else {
-        await knex("ingredients").insert({
-          dish_id: dish.id,
-          name: ingredient,
-        });
-      }
-    }
-
-    return response.json();
+    return response.status(200).json();
   }
 
   async show(request, response) {
